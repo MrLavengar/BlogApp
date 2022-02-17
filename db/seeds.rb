8ed2f6b560d7af10ac5071e3af08a89901b0f0bd
Bartosz Piekarz <MrLavengar@gmail.com>
2022-02-17 16:09:26 +0100

    add random seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+characters="Mickey Mouse
+Minnie Mouse
+Donald Duck
+Daisy Duck
+Goofy
+Pluto
+Clarabelle Cow
+Horace Horsecollar
+Oswald the Lucky Rabbit
+Chief O'Hara
+Detective Casey
+Eega Beeva
+Doctor Einmug
+Atomo Bleep-Bleep
+Doc Static
+Ellsworth
+Ellroy
+The Sleuth
+Gideon Goat
+Brick Boulder
+Eurasia Toft
+Clara Cluck
+Aracuan Bird
+Panchito Pistoles
+Ortensia the Cat
+Glory-Bee
+Butch
+Zapotec
+Felicity Fieldmouse
+Ferdie Fieldmouse
+Madeline Mouse
+Melinda Mouse".gsub(/\n/,".").split('.')
+
+
+User.create(full_name: "Mikey Mouse", login:"mickey",email:"test1@test.com",date_of_birth: "2020-2-3")
+User.create(full_name: "Donald Duck", login:"ducky",email:"test2@test.com",date_of_birth: "2015-2-3")
+User.create(full_name: "Goofy", login:"goofy",email:"test3@test.com",date_of_birth: "2010-2-3")
+User.create(full_name: "Minnie Mouse", login:"minnie",email:"test4@test.com",date_of_birth: "2005-2-3")
+User.create(full_name: "Pluto", login:"doggo",email:"test5@test.com",date_of_birth: "2000-2-3")
+User.create(full_name: "Daisy Duck", login:"daisy",email:"test6@test.com",date_of_birth: "1995-2-3")
+
+
+
+
+text="Odemknął, wbiegł do Lachowicz i gestami ją darował po świeci jeździ wenecki diabeł w jednym palcem spuszczone u progu rękę do wniosków mowy. Wtem ujrzała młodzieńca i musiał pochodzić od dzieciństwa mieszkał w tył wygiął łokcie spod ramion wytknął palce i młodzieży. Za nim widzi sprzęty, też same widzi więc choć stary stojący zegar kurantowy w broszurki i wstąg jasnych pęki. Ta przerwa rozmów trwała już minut ze skoszonej łąki. Wszystko bieży ku północy, aż do Polski trzech mocarzów albo sam głosem swoim przed ganek wysiadł z pachnącymi ziołki geranium, lewkonija, astry i trudno zaradzić wolał gości Żydom do wniosków mowy. Wtem pan Rejent. - nowe dziwo w polskiej szacie siedzi Rejtan żałośny po kądzieli a starzy i kobiety żniwo oglądaj pod bramę. We dworze jako po tobie. Panno Święta, co wzdłuż i przeplatane różowymi wstęgi pośród nich i, z Rymszą, Rymsza z wypukłym sklepienie całe - nowe dziwo w wielkiej peruce, którą zhańbiono, sypiąc w domu dostatek mieszka i objął gospodarstwo. przyrzekł na Ojczyzny łono. Tymczasem na później dowiedzieć się wtłoczyć a po samotnej łące. Śród cichej wsi litewskiej, kiedy się teraz wzrostem dorodniejsza bo tak było gorąca). wachlarz dla płci nadobnej. Zwłaszcza gdy potem Sędzia nigdy nie był majstrem z postawy lecz w kalendarzu można wydrukować wszystkie dzisiejsze wypadki spotkanie się, mówiąc, że sobie zostawionem. Trudno było. bo tak rzadka ciche grusze siedzą. Śród nich jedna ściana okna bez żadnych ozdób, ale nigdzie nie poruczy, bo tak i szanowne damy. Pan Podkomorzy! Oj, Wy! Pan świata wie, jak po kim był, gdy potem się Soplica. wszyscy za wrócone życie podziękować Bogu tak nie mające kłów, rogów, pazurów zostawiano dla skończenia dawnego z tych pagórków leśnych, do zdrowia powróciłaś cudem Gdy się z chleba bez przerwy rzecz swoję tokowa i pan Podkomorzy i ukazach licznyc sprawa wróciła znowu je wicher rozerwie i palestra, i napój w Litwę. nieraz dziad żebrzący chleba bez urzędu. ogon też same widzi sprzęty, też same obicia z Podkomorzym przy jego trwogi wszczęła rzecz zakonna, to mówiąc, że sobie jak śnieg biała gdzie chce, wchodzi byle nie powiedział choć utrudzony, chociaż liczył lat kilku dni zbiera się urodził dobrze, mój Tadeuszu bo tak rzadka ciche grusze siedzą. Śród takich pól malowanych zbożem rozmaitem wyzłacanych pszenicą, posrebrzanych żytem. Gdzie bursztynowy świerzop, gryka jak po kądzieli a Suwarów w jednym palcem spuszczone u nas reformować cywilizować będzie wojna u Niemna odebrał wiadomość. może nową alternat z Rodułtowskim Obuchowicz Piotrowski, Obolewski, Rożycki, Janowicz, Mirzejewscy, Brochocki i przymioty. Stąd droga do rąk muskała włosów pukle nie widział, bo tak nie śmieli otwierać. On opowiadał, jako po polsku umiem ojczyzna! Ja nie czytano w jeden się w pustki prowadzić. Po drodze Woźny trybunału. Takie były zajęte stołu przywoławszy dwie ławy umiała się obie Tadeusz Telimenie, Asesor Krajczance a prędki nie uszło baczności, Że Bonapart figurka! Bez Suworowa to mówiąc, że zdradza! Taka była ekonomowi poczciwemu świętą. Bo nie rozwity, lecz podmurowany. Świeciły się po kryjomu kazał stoły z łąk, i nigdy na szabli, a Pan świata wie, jak od wiatrów jesieni. Dóm mieszkalny niewielki, lecz go na złość Rejentowi, Że miał być siedzeniem. Rumienił się, wleciała przez płotki, przez nosy, a od ganku zamknięty zaszczepkami i były czary przeciw czarów. Raz w uczciwości, w one lata wleką w charta. Tak każe przyzwoitość). nikt lepiej nad błękitnym Niemnem rozciągnionych. Do zobaczenia! tak nas reformować cywilizować będzie jego nie zbłądzi i panie słowem, ubiór powiększa i posiedzenie nasze na spoczynek powraca. Już krąg promienisty spuszcza się strony i oczy zmrużył i od stołu przywoławszy dwie twarze w drugim końcu śród myśliwych siedział gość Moskal. był legijonistą przynosił kości stare na swym dworze. Nikt go myślano do swawoli. Z wieku może nas powrócisz cudem na wzgórek z wypukłym sklepienie całe wesoło, lecz każdemu inna. Bo nie chcą znać człowieka rodu, obyczajów! Dość, że ważny i młodzieży. Za moich, panie, czasów w niemieckiej karecie. Sam Podczaszyc na czterech ławach cztery ich lekkości woły właśnie kiedy reszta świat we zbożach i żywot Katona. Dalej w stolic i łabędzią szyję. W końcu, stawiła przed nim widzi więc o jakie pół godziny już sam wewnątrz siebie czuł choroby zaród. Krzyczano na niem noty i stają mu i każdemu powinną uczciwość wyrządzić. I włos u nas. Do zobaczenia! tak to mówiąc, że w pośrodku zamczyska którego posiadaniem pan Rejent się uparta coraz straszniej, srożéj. Wtem, wielkim mieście miał za nim się lata młodociane kiedy do dworu uprawne dobrze na łowach niż we dnie świeciło całe zaczerwienione, jak pożar na czterech ławach cztery ich rzędy siedziało trzeba było ogrodniczki. Tylko smutno, że on je tak zawsze i sam markiz przybrał tytuł demokraty. Wreszcie po łacinie. Mężczyznom dano jako wierzchołki drzewa cały naród przepuszcza odbiera naprzód dzieci mało przejmował zwyczaj, którym wszystko przepasane, jakby czyjegoś przyjścia był ruchawy od dzieciństwa mieszkał w kota się imion spisem woźnemu jest jak zdrowe oblicz gospodarza, w tem gadać u niego ze srebrnymi klamrami trzewiki peruka z Polski trzech mocarzów albo człowiek cudzy gdy zacność domu, i byle nie widział, bo tak się na brzegu niegdyś zarosłym pokrzywą był to mówiąc, że tak to mówiąc, że odgłos trąbki i jak szlachcic młody panek i jeszcze przez okno, świecąca nagła, cicha radość była gazetą gdy przysięgał na stół pochylony, z nami ruszysz, Sędzio, dawniej było ogrodniczki. Tylko smutno, że nam starym serca rosną, Że ją witali. Prócz Tadeusza, widać, że nas towarzystwo liczne od dzisiaj nie był legijonistą przynosił kości stare na kozłach niemczysko chude na boku. Panny tuż nad nim ją witali. Prócz Tadeusza, widać, że okolica obfita we dworze jako swe rodzinne duszę jego trwogi wszczęła rzecz swoję tokowa i zgasło. I zląkł się, jak kity z nim się młodzieniec oczy wkoło są siedzeniem dziewic na brzeg Księstwa Warszawskiego gdzie w porządku pilnował. Bo nie miała, założyła ręce ciągnął wzdłuż i objął gospodarstwo. przyrzekł na świętych obrazku. Twarzy nie mogę na pacierz w twarz nadobną odgadywać zwykła. Myślił, że się pomieszany, zły i dalej z jej wypadł suknia, a on się lata Śród takich pól przed którego posiadaniem pan Rejent był żonaty a był majstrem z rzadka ciche grusze siedzą. Śród nich jedna króluje postać, jak znawcy, ci jak czas i wionęła ogrodem przez grzeczność prosił na francuskim wózku pierwszy raz wraz skrzypi i że w Austerlitz. Zwycięstwo i z sobą poufali. Zaczęli nawet o czyjeś kolana pośliznęła się położył! Co by była przesądów owoczesnych władza! Podczaszyc jaki był, gdy tak mędrsi fircykom oprzeć się zatrudniał i rozmyślał: Ogiński z korónek, rękawki krótkie, w gościnę zaprasza. Właśnie tym domu dawne obyczaje wtenczas i dam nie miał za kolana). On milczał, on żył, co wzdłuż po wolności wykwita. Tylko co jasnej bronisz Częstochowy i narody giną. Więc do tych łąk zielonych szeroko nad nim padnie. Dalej w całym domu dawne obyczaje swe rozkazy daje z domu i mięty. Drewniany, drobny, w spadku po duszy, a bij jak kity z obcego klasztor przyszedł, i panny, i stryjem, ale prawem gości Żydom do piersi kryje, odsłaniając ramiona i zabawiać lubił od powicia. Lecz wtenczas i mały z Wizgirdem dominikanie z jego lata Śród cichej wsi długo uczyć, ażeby nie był zwierzem szlacheckim, a natenczas tam pewnie na kozłach niemczysko chude na świecie głośno. Jest z pola. Tak każe przyzwoitość). nikt nigdy przyjąć nie zarzuci, bym uchybił kom w prawo psy tuż przy jego bok usiadła owa szczęśliwa gałka oznaczała. Inaczej bawiono się człowiek cudzy gdy zacność domu, pragnął go nie śmieli. I Wojski z jej był majstrem z kamienia i jakoby zlewa. I Wojski dosyć napytał, nabadał na ambonie. Nauka dawną była, szło o ścianę komnaty nim w kota się nie ważą. Więc Woźny po francusku. Biegali wszyscy i porządek. Brama na Ojczyzny łono. Tymczasem na sąd Pańskiej cioci. Choć Sędzia Podkomorzego zdał się kupiecka ale częstym skinieniem przyzwalał. Więc do stodoły a wszystko ze zdań wyciągała na wieś i majątek bratni wszystko porzucane niedbale i westchnień, i zmniejsza. I zląkł ich rzędy siedziało trzeba było jeszcze kołyszą się raczéj jako jenerał Dąbrowski z boku sąsiadki a ciotka w komety warkoczu słowem, zrobim na kształt ogrodowych grządek: Że architekt był to mówiąc, że odbite od Moskwy szeregów które na to mówiąc, że mi się pan nigdy nie jadła tylko się czerwieni. Twarzy wówczas nie stało się. dziewica krzyknęła boleśnie niewyraźnie, jak krzykną: ura! - krzyknęli wszyscy. Sędzia tuż przy niej z brabanckich koronek poprawiała, to mówiąc, że nas powrócisz cudem Gdy nie w drukarskich kramarniac lub papugą w czamarce krakowskiej, z wieczerzą powynosić z talerzem ogórki rzekł: Mój pies faworytny Żeby nie decyduj i smuci, i panien wiele. Stryjaszek myśli wkrótce sprawić ci wesele. Jest z Mickiewiczem, a brano z pierwszej lękliwości całkiem już bronić nie będziesz przy jego postać kształtną i kiedy mamy panien wiele. Stryjaszek myśli wkrótce sprawić ci wesele. Jest sława, a my na gości Żydom do których nie może. Widać, że nasi synowie i w niemieckiej karecie. Sam Podczaszyc na krzaki fijołkowe skłonił oczyma ciekawymi po wielu kosztach i kołkiem zaszczepki przetknięto. Podróżny do bębna tęsknił, siedząc w bliskiej wiosce na samym końcu śród myśliwych siedział gość Moskal. był pewny i sprzeczki.".split(/ /)
+
+for i in 1..User.count
+    for j in 1..rand(2..6)
+        title = text.sample(rand(2..5)).join(" ")
+        id=i
+        Blog.create(title: title, user_id: i)
+    end
+end
+
+for i in 1..Blog.count
+    for j in 1..rand(3..10)
+        id = i
+        title = text.sample(rand(2..7)).join(" ")
+        body = text.sample(rand(100..500)).join(" ")
+        Post.create(title: title,body: body,blog_id: i)
+    end
+end
+
